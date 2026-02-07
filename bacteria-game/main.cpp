7eaf0ae60de4ab4e733ab615d6d55be476631924
egor <egor1999x@mail.ru>
2026-02-07 17:03:54 +0300

    moved impl

--- bacteria-game/main.cpp
+++ bacteria-game/main.cpp
@@ -1,65 +1,16 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Window/Keyboard.hpp>
+#include <thread>
 
-#include <vector>
-#include <iostream>
-#include <set>
-#include <string>
-
-
-template<typename T>
-std::ostream& operator <<(std::ostream& stream, const std::set<T>& v)
-{
-    stream << '[';
-    for (auto it = v.cbegin(); it != v.cend(); ++it)
-    {
-        stream << *it;
-        //if (it + 1 != v.cend())
-            stream << ',';
-    }
-    stream << ']' << std::endl;
-    return stream;
-}
-
-std::ostream& operator <<(std::ostream& stream, const sf::Keyboard::Scancode& v)
-{
-    auto str = sf::Keyboard::getDescription(v).toAnsiString();
-    return stream << str;
-}
-
-class KeyboardHandler
-{
-    std::set<sf::Keyboard::Scancode> m_pressed_key_array;
-public:
-    void keyPressed(const std::optional<sf::Event> &e)
-    {
-        auto key_press = e->getIf<sf::Event::KeyPressed>();
-        if (m_pressed_key_array.find(key_press->scancode) == m_pressed_key_array.end())
-        {
-            m_pressed_key_array.insert(key_press->scancode);
-            std::cout << m_pressed_key_array << std::endl;
-        }
-        
-    }
-
-    void keyReleased(const std::optional<sf::Event>& e)
-    {
-        auto key_release = e->getIf<sf::Event::KeyReleased>();
-        m_pressed_key_array.erase(m_pressed_key_array.find(key_release->scancode));
-        std::cout << m_pressed_key_array << std::endl;
-    }
-};
+#include "render.h"
+#include "keyboardhandler.h"
 
 int main()
 {
-    std::vector<int> v{1, 2, 3, 4, 5};
+    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "SFML 3 Window");
+    window.setActive(false);
 
-    //std::cout << v << std::endl;
+    std::thread thread(&renderingThread, &window);
 
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML 3 Window");
     KeyboardHandler keyboard_handler;
-
-
     while (window.isOpen())
     {
         while (auto event = window.pollEvent())
@@ -78,9 +29,8 @@ int main()
             else if (event->is<sf::Event::KeyReleased>())
                 keyboard_handler.keyReleased(event);
         }
-
-        window.clear({0,0,255});
-        window.display();
     }
+
+    thread.join();
 }
 
